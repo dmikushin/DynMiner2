dfd4ac580ec4f0e9a4c2f2a47a359aed5d802000
Dmitry Mikushin <dmitry@kernelgen.org>
2022-02-05 12:14:25 +0100

    Formatting the code, making notes

--- src/sha_table.cuh
+++ src/sha_table.cuh
@@ -79,201 +79,191 @@
 
 __constant uint k_sha256[64] =
 {
-  SHA256C00, SHA256C01, SHA256C02, SHA256C03,
-  SHA256C04, SHA256C05, SHA256C06, SHA256C07,
-  SHA256C08, SHA256C09, SHA256C0a, SHA256C0b,
-  SHA256C0c, SHA256C0d, SHA256C0e, SHA256C0f,
-  SHA256C10, SHA256C11, SHA256C12, SHA256C13,
-  SHA256C14, SHA256C15, SHA256C16, SHA256C17,
-  SHA256C18, SHA256C19, SHA256C1a, SHA256C1b,
-  SHA256C1c, SHA256C1d, SHA256C1e, SHA256C1f,
-  SHA256C20, SHA256C21, SHA256C22, SHA256C23,
-  SHA256C24, SHA256C25, SHA256C26, SHA256C27,
-  SHA256C28, SHA256C29, SHA256C2a, SHA256C2b,
-  SHA256C2c, SHA256C2d, SHA256C2e, SHA256C2f,
-  SHA256C30, SHA256C31, SHA256C32, SHA256C33,
-  SHA256C34, SHA256C35, SHA256C36, SHA256C37,
-  SHA256C38, SHA256C39, SHA256C3a, SHA256C3b,
-  SHA256C3c, SHA256C3d, SHA256C3e, SHA256C3f,
+	SHA256C00, SHA256C01, SHA256C02, SHA256C03,
+	SHA256C04, SHA256C05, SHA256C06, SHA256C07,
+	SHA256C08, SHA256C09, SHA256C0a, SHA256C0b,
+	SHA256C0c, SHA256C0d, SHA256C0e, SHA256C0f,
+	SHA256C10, SHA256C11, SHA256C12, SHA256C13,
+	SHA256C14, SHA256C15, SHA256C16, SHA256C17,
+	SHA256C18, SHA256C19, SHA256C1a, SHA256C1b,
+	SHA256C1c, SHA256C1d, SHA256C1e, SHA256C1f,
+	SHA256C20, SHA256C21, SHA256C22, SHA256C23,
+	SHA256C24, SHA256C25, SHA256C26, SHA256C27,
+	SHA256C28, SHA256C29, SHA256C2a, SHA256C2b,
+	SHA256C2c, SHA256C2d, SHA256C2e, SHA256C2f,
+	SHA256C30, SHA256C31, SHA256C32, SHA256C33,
+	SHA256C34, SHA256C35, SHA256C36, SHA256C37,
+	SHA256C38, SHA256C39, SHA256C3a, SHA256C3b,
+	SHA256C3c, SHA256C3d, SHA256C3e, SHA256C3f,
 };
 
-#define SHA256_STEP(F0a,F1a,a,b,c,d,e,f,g,h,x,K)        \
-{                                                       \
-  h += K + x;                                           \
-  h += S3 (e) + F1a (e,f,g);                            \
-  d += h;                                               \
-  h += S2 (a) + F0a (a,b,c);                            \
-}                                                       \
-
-
+#define SHA256_STEP(F0a,F1a,a,b,c,d,e,f,g,h,x,K) \
+{ \
+	h += K + x; \
+	h += S3 (e) + F1a (e,f,g); \
+	d += h; \
+	h += S2 (a) + F0a (a,b,c); \
+} \
 
 #define SHA256_EXPAND(x,y,z,w) (S1 (x) + y + S0 (z) + w) 
 
-static void sha256_process2 (const unsigned int *W, unsigned int *digest)
-{
-  unsigned int a = digest[0];
-  unsigned int b = digest[1];
-  unsigned int c = digest[2];
-  unsigned int d = digest[3];
-  unsigned int e = digest[4];
-  unsigned int f = digest[5];
-  unsigned int g = digest[6];
-  unsigned int h = digest[7];
-
-  unsigned int w0_t = W[0];
-  unsigned int w1_t = W[1];
-  unsigned int w2_t = W[2];
-  unsigned int w3_t = W[3];
-  unsigned int w4_t = W[4];
-  unsigned int w5_t = W[5];
-  unsigned int w6_t = W[6];
-  unsigned int w7_t = W[7];
-  unsigned int w8_t = W[8];
-  unsigned int w9_t = W[9];
-  unsigned int wa_t = W[10];
-  unsigned int wb_t = W[11];
-  unsigned int wc_t = W[12];
-  unsigned int wd_t = W[13];
-  unsigned int we_t = W[14];
-  unsigned int wf_t = W[15];
-
-  #define ROUND_EXPAND(i)                           \
-  {                                                 \
-    w0_t = SHA256_EXPAND (we_t, w9_t, w1_t, w0_t);  \
-    w1_t = SHA256_EXPAND (wf_t, wa_t, w2_t, w1_t);  \
-    w2_t = SHA256_EXPAND (w0_t, wb_t, w3_t, w2_t);  \
-    w3_t = SHA256_EXPAND (w1_t, wc_t, w4_t, w3_t);  \
-    w4_t = SHA256_EXPAND (w2_t, wd_t, w5_t, w4_t);  \
-    w5_t = SHA256_EXPAND (w3_t, we_t, w6_t, w5_t);  \
-    w6_t = SHA256_EXPAND (w4_t, wf_t, w7_t, w6_t);  \
-    w7_t = SHA256_EXPAND (w5_t, w0_t, w8_t, w7_t);  \
-    w8_t = SHA256_EXPAND (w6_t, w1_t, w9_t, w8_t);  \
-    w9_t = SHA256_EXPAND (w7_t, w2_t, wa_t, w9_t);  \
-    wa_t = SHA256_EXPAND (w8_t, w3_t, wb_t, wa_t);  \
-    wb_t = SHA256_EXPAND (w9_t, w4_t, wc_t, wb_t);  \
-    wc_t = SHA256_EXPAND (wa_t, w5_t, wd_t, wc_t);  \
-    wd_t = SHA256_EXPAND (wb_t, w6_t, we_t, wd_t);  \
-    we_t = SHA256_EXPAND (wc_t, w7_t, wf_t, we_t);  \
-    wf_t = SHA256_EXPAND (wd_t, w8_t, w0_t, wf_t);  \
-  }
-
-  #define ROUND_STEP(i)                                                                   \
-  {                                                                                       \
-    SHA256_STEP (F0, F1, a, b, c, d, e, f, g, h, w0_t, k_sha256[i +  0]); \
-    SHA256_STEP (F0, F1, h, a, b, c, d, e, f, g, w1_t, k_sha256[i +  1]); \
-    SHA256_STEP (F0, F1, g, h, a, b, c, d, e, f, w2_t, k_sha256[i +  2]); \
-    SHA256_STEP (F0, F1, f, g, h, a, b, c, d, e, w3_t, k_sha256[i +  3]); \
-    SHA256_STEP (F0, F1, e, f, g, h, a, b, c, d, w4_t, k_sha256[i +  4]); \
-    SHA256_STEP (F0, F1, d, e, f, g, h, a, b, c, w5_t, k_sha256[i +  5]); \
-    SHA256_STEP (F0, F1, c, d, e, f, g, h, a, b, w6_t, k_sha256[i +  6]); \
-    SHA256_STEP (F0, F1, b, c, d, e, f, g, h, a, w7_t, k_sha256[i +  7]); \
-    SHA256_STEP (F0, F1, a, b, c, d, e, f, g, h, w8_t, k_sha256[i +  8]); \
-    SHA256_STEP (F0, F1, h, a, b, c, d, e, f, g, w9_t, k_sha256[i +  9]); \
-    SHA256_STEP (F0, F1, g, h, a, b, c, d, e, f, wa_t, k_sha256[i + 10]); \
-    SHA256_STEP (F0, F1, f, g, h, a, b, c, d, e, wb_t, k_sha256[i + 11]); \
-    SHA256_STEP (F0, F1, e, f, g, h, a, b, c, d, wc_t, k_sha256[i + 12]); \
-    SHA256_STEP (F0, F1, d, e, f, g, h, a, b, c, wd_t, k_sha256[i + 13]); \
-    SHA256_STEP (F0, F1, c, d, e, f, g, h, a, b, we_t, k_sha256[i + 14]); \
-    SHA256_STEP (F0, F1, b, c, d, e, f, g, h, a, wf_t, k_sha256[i + 15]); \
-  }
-
-  ROUND_STEP (0);
-
-  ROUND_EXPAND();
-  ROUND_STEP(16);
-
-  ROUND_EXPAND();
-  ROUND_STEP(32);
-
-  ROUND_EXPAND();
-  ROUND_STEP(48);
-
-  digest[0] += a;
-  digest[1] += b;
-  digest[2] += c;
-  digest[3] += d;
-  digest[4] += e;
-  digest[5] += f;
-  digest[6] += g;
-  digest[7] += h;
+#define ROUND_EXPAND(i) \
+{ \
+	w0_t = SHA256_EXPAND (we_t, w9_t, w1_t, w0_t);  \
+	w1_t = SHA256_EXPAND (wf_t, wa_t, w2_t, w1_t);  \
+	w2_t = SHA256_EXPAND (w0_t, wb_t, w3_t, w2_t);  \
+	w3_t = SHA256_EXPAND (w1_t, wc_t, w4_t, w3_t);  \
+	w4_t = SHA256_EXPAND (w2_t, wd_t, w5_t, w4_t);  \
+	w5_t = SHA256_EXPAND (w3_t, we_t, w6_t, w5_t);  \
+	w6_t = SHA256_EXPAND (w4_t, wf_t, w7_t, w6_t);  \
+	w7_t = SHA256_EXPAND (w5_t, w0_t, w8_t, w7_t);  \
+	w8_t = SHA256_EXPAND (w6_t, w1_t, w9_t, w8_t);  \
+	w9_t = SHA256_EXPAND (w7_t, w2_t, wa_t, w9_t);  \
+	wa_t = SHA256_EXPAND (w8_t, w3_t, wb_t, wa_t);  \
+	wb_t = SHA256_EXPAND (w9_t, w4_t, wc_t, wb_t);  \
+	wc_t = SHA256_EXPAND (wa_t, w5_t, wd_t, wc_t);  \
+	wd_t = SHA256_EXPAND (wb_t, w6_t, we_t, wd_t);  \
+	we_t = SHA256_EXPAND (wc_t, w7_t, wf_t, we_t);  \
+	wf_t = SHA256_EXPAND (wd_t, w8_t, w0_t, wf_t);  \
 }
 
+#define ROUND_STEP(i) \
+{ \
+	SHA256_STEP (F0, F1, a, b, c, d, e, f, g, h, w0_t, k_sha256[i +  0]); \
+	SHA256_STEP (F0, F1, h, a, b, c, d, e, f, g, w1_t, k_sha256[i +  1]); \
+	SHA256_STEP (F0, F1, g, h, a, b, c, d, e, f, w2_t, k_sha256[i +  2]); \
+	SHA256_STEP (F0, F1, f, g, h, a, b, c, d, e, w3_t, k_sha256[i +  3]); \
+	SHA256_STEP (F0, F1, e, f, g, h, a, b, c, d, w4_t, k_sha256[i +  4]); \
+	SHA256_STEP (F0, F1, d, e, f, g, h, a, b, c, w5_t, k_sha256[i +  5]); \
+	SHA256_STEP (F0, F1, c, d, e, f, g, h, a, b, w6_t, k_sha256[i +  6]); \
+	SHA256_STEP (F0, F1, b, c, d, e, f, g, h, a, w7_t, k_sha256[i +  7]); \
+	SHA256_STEP (F0, F1, a, b, c, d, e, f, g, h, w8_t, k_sha256[i +  8]); \
+	SHA256_STEP (F0, F1, h, a, b, c, d, e, f, g, w9_t, k_sha256[i +  9]); \
+	SHA256_STEP (F0, F1, g, h, a, b, c, d, e, f, wa_t, k_sha256[i + 10]); \
+	SHA256_STEP (F0, F1, f, g, h, a, b, c, d, e, wb_t, k_sha256[i + 11]); \
+	SHA256_STEP (F0, F1, e, f, g, h, a, b, c, d, wc_t, k_sha256[i + 12]); \
+	SHA256_STEP (F0, F1, d, e, f, g, h, a, b, c, wd_t, k_sha256[i + 13]); \
+	SHA256_STEP (F0, F1, c, d, e, f, g, h, a, b, we_t, k_sha256[i + 14]); \
+	SHA256_STEP (F0, F1, b, c, d, e, f, g, h, a, wf_t, k_sha256[i + 15]); \
+}
+
+static void sha256_process2 (const unsigned int *W, unsigned int *digest)
+{
+	unsigned int a = digest[0];
+	unsigned int b = digest[1];
+	unsigned int c = digest[2];
+	unsigned int d = digest[3];
+	unsigned int e = digest[4];
+	unsigned int f = digest[5];
+	unsigned int g = digest[6];
+	unsigned int h = digest[7];
+
+	unsigned int w0_t = W[0];
+	unsigned int w1_t = W[1];
+	unsigned int w2_t = W[2];
+	unsigned int w3_t = W[3];
+	unsigned int w4_t = W[4];
+	unsigned int w5_t = W[5];
+	unsigned int w6_t = W[6];
+	unsigned int w7_t = W[7];
+	unsigned int w8_t = W[8];
+	unsigned int w9_t = W[9];
+	unsigned int wa_t = W[10];
+	unsigned int wb_t = W[11];
+	unsigned int wc_t = W[12];
+	unsigned int wd_t = W[13];
+	unsigned int we_t = W[14];
+	unsigned int wf_t = W[15];
+
+	ROUND_STEP (0);
+
+	ROUND_EXPAND();
+	ROUND_STEP(16);
+
+	ROUND_EXPAND();
+	ROUND_STEP(32);
+
+	ROUND_EXPAND();
+	ROUND_STEP(48);
+
+	digest[0] += a;
+	digest[1] += b;
+	digest[2] += c;
+	digest[3] += d;
+	digest[4] += e;
+	digest[5] += f;
+	digest[6] += g;
+	digest[7] += h;
+}
 
 static void sha256 ( uint pass_len,  const unsigned int *pass,  uint *hash) 
-{                 
+{				 
+	int plen=pass_len/4;			
+	if (mod(pass_len,4)) plen++;	
+	
+	unsigned int* p = hash; 
+									
+	unsigned int W[0x10]={0};   
+	int loops=plen;			 
+	int curloop=0;			  
+	unsigned int State[8]={0};  
+	State[0] = 0x6a09e667;	  
+	State[1] = 0xbb67ae85;	  
+	State[2] = 0x3c6ef372;	  
+	State[3] = 0xa54ff53a;	  
+	State[4] = 0x510e527f;	  
+	State[5] = 0x9b05688c;	  
+	State[6] = 0x1f83d9ab;	  
+	State[7] = 0x5be0cd19;	  
+						
+	while (loops>0)	 
+	{				   
+		W[0x0]=0x0;	 
+		W[0x1]=0x0;	 
+		W[0x2]=0x0;	 
+		W[0x3]=0x0;	 
+		W[0x4]=0x0;	 
+		W[0x5]=0x0;	 
+		W[0x6]=0x0;	 
+		W[0x7]=0x0;	 
+		W[0x8]=0x0;	 
+		W[0x9]=0x0;	 
+		W[0xA]=0x0;	 
+		W[0xB]=0x0;	 
+		W[0xC]=0x0;	 
+		W[0xD]=0x0;	 
+		W[0xE]=0x0;	 
+		W[0xF]=0x0;	 
+						
+		for (int m=0;loops!=0 && m<16;m++)	  
+		{									   
+			W[m]^=SWAP(pass[m+(curloop*16)]);   
+			loops--;							
+		}							
+														
+		if (loops==0 && mod(pass_len,64)!=0)	
+		{									   
+			unsigned int padding=0x80<<(((pass_len+4)-((pass_len+4)/4*4))*8);   
+			int v=mod(pass_len,64);		 
+			W[v/4]|=SWAP(padding);		  
+			if ((pass_len&0x3B)!=0x3B)	  
+			{							   
+				W[0x0F]=pass_len*8;		 
+			}							   
+		}								   
+										
+		sha256_process2(W,State);	   
+		curloop++;					  
+	}								   
 
-    int plen=pass_len/4;            
-    if (mod(pass_len,4)) plen++;    
-    
-    unsigned int* p = hash; 
-                                    
-    unsigned int W[0x10]={0};   
-    int loops=plen;             
-    int curloop=0;              
-    unsigned int State[8]={0};  
-    State[0] = 0x6a09e667;      
-    State[1] = 0xbb67ae85;      
-    State[2] = 0x3c6ef372;      
-    State[3] = 0xa54ff53a;      
-    State[4] = 0x510e527f;      
-    State[5] = 0x9b05688c;      
-    State[6] = 0x1f83d9ab;      
-    State[7] = 0x5be0cd19;      
-                        
-    while (loops>0)     
-    {                   
-        W[0x0]=0x0;     
-        W[0x1]=0x0;     
-        W[0x2]=0x0;     
-        W[0x3]=0x0;     
-        W[0x4]=0x0;     
-        W[0x5]=0x0;     
-        W[0x6]=0x0;     
-        W[0x7]=0x0;     
-        W[0x8]=0x0;     
-        W[0x9]=0x0;     
-        W[0xA]=0x0;     
-        W[0xB]=0x0;     
-        W[0xC]=0x0;     
-        W[0xD]=0x0;     
-        W[0xE]=0x0;     
-        W[0xF]=0x0;     
-                        
-        for (int m=0;loops!=0 && m<16;m++)      
-        {                                       
-            W[m]^=SWAP(pass[m+(curloop*16)]);   
-            loops--;                            
-        }                            
-        
-                                                        
-        if (loops==0 && mod(pass_len,64)!=0)    
-        {                                       
-            unsigned int padding=0x80<<(((pass_len+4)-((pass_len+4)/4*4))*8);   
-            int v=mod(pass_len,64);         
-            W[v/4]|=SWAP(padding);          
-            if ((pass_len&0x3B)!=0x3B)      
-            {                               
-                W[0x0F]=pass_len*8;         
-            }                               
-        }                                   
-                                        
-        sha256_process2(W,State);       
-        curloop++;                      
-    }                                   
-             
-
-                            
-    p[0]=SWAP(State[0]);    
-    p[1]=SWAP(State[1]);    
-    p[2]=SWAP(State[2]);    
-    p[3]=SWAP(State[3]);    
-    p[4]=SWAP(State[4]);    
-    p[5]=SWAP(State[5]);    
-    p[6]=SWAP(State[6]);    
-    p[7]=SWAP(State[7]);    
-  
-
-    return;                 
+	p[0]=SWAP(State[0]);	
+	p[1]=SWAP(State[1]);	
+	p[2]=SWAP(State[2]);	
+	p[3]=SWAP(State[3]);	
+	p[4]=SWAP(State[4]);	
+	p[5]=SWAP(State[5]);	
+	p[6]=SWAP(State[6]);	
+	p[7]=SWAP(State[7]);	
 }
 
 #endif // SHA_TABLE_CUH
